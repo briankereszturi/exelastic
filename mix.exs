001077da7df6e457ae316af966c589e36de1d14a
Brian Kereszturi <briankereszturi@gmail.com>
2017-07-24 16:07:00 -0700

    Go back to upstream master.

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule ExElasticHelpers.Mixfile do
 
   defp deps do
     [
-      {:elastix, git: "https://github.com/briankereszturi/elastix.git", branch: "delete-by-query"}
+      {:elastix, git: "https://github.com/werbitzky/elastix.git"}
     ]
   end
 end
